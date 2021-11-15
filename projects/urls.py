f40f0053895dfd82e8594bf58d44717e68f66943
Lucas C. Bittencourt <lucascafieiro@gmail.com>
2021-11-15 17:21:09 -0300

    Theme Installation

--- projects/urls.py
+++ projects/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.list, name="projects_list"),
-  path('find-project/<str:pk>/', views.index, name="projects_index"),
-  path('new-project/', views.create, name="projects_create"),
-  path('update-project/<str:pk>', views.update, name="projects_update"),
-  path('delete-project/<str:pk>', views.delete, name="projects_delete")
+    path('', views.list, name="projects_list"),
+    path('find-project/<str:pk>/', views.index, name="projects_index"),
+    path('new-project/', views.create, name="projects_create"),
+    path('update-project/<str:pk>', views.update, name="projects_update"),
+    path('delete-project/<str:pk>', views.delete, name="projects_delete")
 ]
