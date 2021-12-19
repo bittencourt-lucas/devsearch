4ea01c7383ba41869ab9311ecefe1067a9dd2fd0
Lucas Bittencourt <lucas.bittencourt@trio.dev>
2021-12-19 19:33:40 -0300

    Signals

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
