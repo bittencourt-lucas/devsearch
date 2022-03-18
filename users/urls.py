8681a0dc6091a31eebaa7f4aabcf027cc7a0c94e
Lucas Bittencourt <lucas.bittencourt@optelgroup.com>
2022-03-17 23:13:11 -0300

    User Account

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.registerUser, name="register"),
     path('', views.profiles, name="profiles_list"),
     path('profile/<str:pk>/', views.user_profile, name="user_profile"),
+    path('account/', views.user_account, name="account"),
 ]
